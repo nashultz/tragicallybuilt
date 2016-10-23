ad68c6282e991eb0e3f618cd0dc458aa29468b91
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-10-23 00:42:27 -0500

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,6 +30,7 @@
                 </ul>
             </nav>
         </div>
+        <script src="../../themes/default/assets/js/app.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
     </body>
 </html>
