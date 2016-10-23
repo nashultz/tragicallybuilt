60174a42920ea54b3a0c3fbb685f715ee4dd4001
Nathon Shultz <nathon.shultz@systemsedgeonline.com>
2016-10-23 00:44:04 -0500

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,6 +31,7 @@
             </nav>
         </div>
         <script src="../../themes/default/assets/js/app.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
     </body>
 </html>
